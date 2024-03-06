702687f02df05c9c72e41c38f6739b98caee53c5
MatiSandoval <matisandoval.97@gmail.com>
2024-03-05 21:46:54 -0300

    5/03

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1, imgUrl: "assets/certificates/cert1.png"
+  },
+  {
+    id: 2, imgUrl: "assets/certificates/frontend-argentinaprograma.png"
+  },
+  {
+    id: 3, imgUrl: "assets/certificates/info2.png"
+  },
+  {
+    id: 4, imgUrl: "assets/certificates/unity.png"
+  }
+];
